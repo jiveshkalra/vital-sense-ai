1f72e9aa46cff02a88eb35d348d16ce99ce4b9c9
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-11-30 18:43:50 +0530

    Update get_example_files_list function to use the correct audio files directory path; enhance Next.js configuration for output file tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+      outputFileTracingIncludes: {
+        '/actions/get_example_files_list': ['./public/src/audio_files/*'],
+      },
+    },
+  };
 
 export default nextConfig;
