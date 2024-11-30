941a57408fd31ab9ed7f00c9f0c12c2ffdc01c15
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-11-30 18:00:07 +0530

    Fix audio files directory path in get_example_files_list function

--- app/actions/get_example_files_list.jsx
+++ app/actions/get_example_files_list.jsx
@@ -1,11 +1,10 @@
 'use server'
 import path from 'path'
-import fs from 'fs'
-import FormData from 'form-data'
+import fs from 'fs' 
 
 export default async function get_example_files_list() {
 
-    const audioFilesDir = path.join(process.cwd(),'public', 'src', 'audio_files')
+    const audioFilesDir = path.join('public', 'src', 'audio_files')
     const public_url_dir = path.join('src', 'audio_files')
     const files = fs.readdirSync(audioFilesDir) 
     files.map((file, index) => { 
