0ce99dca8a3569bb0beecf170d7d642019a30261
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-11-30 18:21:49 +0530

    Update get_example_files_list.jsx

--- app/actions/get_example_files_list.jsx
+++ app/actions/get_example_files_list.jsx
@@ -4,7 +4,7 @@ import fs from 'fs'
 
 export default async function get_example_files_list() {
 
-    const audioFilesDir = path.join('public', 'src', 'audio_files')
+    const audioFilesDir = path.join('src', 'audio_files')
     const public_url_dir = path.join('src', 'audio_files')
     const files = fs.readdirSync(audioFilesDir) 
     files.map((file, index) => { 
